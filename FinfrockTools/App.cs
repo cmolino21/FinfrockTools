123bdeb5b201e2a14d09f646b7cd512ee6b42cbc
Christian Molino <cmolino@finfrock.com>
2024-08-19 15:23:27 -0400

    -Updated the ribbon images -Set the Dualdeck icons to only be enabled when in a dualdeck document

--- FinfrockTools/App.cs
+++ FinfrockTools/App.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using System.IO;
 
 namespace FinfrockTools
 {
@@ -19,21 +21,32 @@ namespace FinfrockTools
             application.CreateRibbonTab(tabName);
 
             // Create a ribbon panel within the custom tab
-            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Commands");
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "DualDecks");
 
             // Add the first button
             string firstCommandAssembly = @"J:\Autodesk Standards\Revit\Add-Ins\DualDeckEditor\2023\DualDeckEditorAddin.dll";
             PushButtonData firstButtonData = new PushButtonData("DualDeck Editor", "DualDeck \nEditor", firstCommandAssembly, "DualDeckEditorAddin.DualDeckEditorCommand");
             PushButton firstButton = ribbonPanel.AddItem(firstButtonData) as PushButton;
             firstButton.ToolTip = "Edit DualDeck Parameters.";
-            firstButton.LargeImage = new BitmapImage(new Uri("pack://application:,,,/FinfrockTools;component/Images/1_32.png"));
+            firstButton.LargeImage = new BitmapImage(new Uri("pack://application:,,,/FinfrockTools;component/Images/DD_32.png"));
 
             // Add the second button
             string secondCommandAssembly = @"J:\Autodesk Standards\Revit\Add-Ins\CleanFileAutomation\2023\CleanFileAutomation.dll";
             PushButtonData secondButtonData = new PushButtonData("Clean File Exporter", "Clean File \nExporter", secondCommandAssembly, "CleanFileAutomation.CleanFileCommand");
             PushButton secondButton = ribbonPanel.AddItem(secondButtonData) as PushButton;
             secondButton.ToolTip = "Automatically Export Clean Files.";
-            secondButton.LargeImage = new BitmapImage(new Uri("pack://application:,,,/FinfrockTools;component/Images/1_32.png"));
+            secondButton.LargeImage = new BitmapImage(new Uri("pack://application:,,,/FinfrockTools;component/Images/EX_32.png"));
+
+            // Handle the document opened event to check for the DualDeck model
+            application.ControlledApplication.DocumentOpened += (sender, args) =>
+            {
+                Document doc = args.Document;
+
+                // Disable the buttons if the document title does not contain "DUALDECK"
+                bool isDualDeckModel = doc != null && doc.Title.ToUpper().Contains("DUALDECK");
+                firstButton.Enabled = isDualDeckModel;
+                secondButton.Enabled = isDualDeckModel;
+            };
 
             return Result.Succeeded;
         }
